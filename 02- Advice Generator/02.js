1533d0b53598b005e49aa73dfa5f8af52aa467c1
wqais <warekarqais@gmail.com>
2022-02-22 09:52:24 +0530

    Fixed design issues
    1-  Fixed HTML accessibility issues for website and projects
    
    2- Added Front End Mentor profile link to main website

--- 02- Advice Generator/02.js
+++ 02- Advice Generator/02.js
@@ -1,23 +1,23 @@
 const adviceText = document.querySelector("#advice");
 const getAdvice = document.querySelector(".generate-advice-btn");
 const adviceId = document.querySelector("#advice-id");
-const errorText = document.querySelector('#error');
-const URL = "https://api.adviceslip.com/advice"
+const errorText = document.querySelector("#error");
+const URL = "https://api.adviceslip.com/advice";
 
-
-async function generateAdvice(){
-    try{
-        fetch(URL)
-        .then((result) => result.json())
-        .then((data) =>
-        (adviceId.textContent = data.slip.id) &&
-        (adviceText.textContent= data.slip.advice));
-    }
-    catch(error){
-        adviceText.classList.add("hide");
-        errorText.classList.remove("hide");
-        errorText.textContent = error;
-    }
+async function generateAdvice() {
+  try {
+    fetch(URL)
+      .then((result) => result.json())
+      .then(
+        (data) =>
+          (adviceId.textContent = data.slip.id) &&
+          (adviceText.textContent = data.slip.advice)
+      );
+  } catch (error) {
+    adviceText.classList.add("hide");
+    errorText.classList.remove("hide");
+    errorText.textContent = error;
+  }
 }
 
-getAdvice.addEventListener("click", generateAdvice)
+getAdvice.addEventListener("click", generateAdvice);
